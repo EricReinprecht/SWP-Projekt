4bc6f29f3dcc12d1fa7d63f4ee84c12618969dd4
EricReinprecht <eric.reinprecht@student.htldornbirn.at>
2022-04-07 11:28:08 +0200

    Variable für input erstellt

--- src/components/inputfield.js
+++ src/components/inputfield.js
@@ -3,10 +3,32 @@ import styles from './inputfield.module.css';
 
 
 class InputField extends Component{
+
+    constructor(props){
+        super(props);
+        this.state={
+            inputValue:""
+        }
+        
+    }
+
+    handleChange = (event)=>{
+        this.setState({
+            inputValue: event.target.value
+        })
+    }
+
+    searchWether = ()=>{
+        if(this.state.inputValue == "Reuthe"){
+            console.log("hallo")
+        }
+    }
+
     render(){
         return(
             <div className={styles.inputfield}>
-                <input type="text" id="input"></input>
+                <input type='text' value={this.state.inputValue} onChange={this.handleChange}/>
+                <button onClick={this.searchWether}>Search</button>
             </div>
         );
     }
