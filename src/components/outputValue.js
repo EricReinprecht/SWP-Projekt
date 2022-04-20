c8a9fb1bf8ad7ec84edf478b2f1f177de4f24cc0
zech.emanuel <emanuel.zech@gmail.com>
2022-04-20 22:42:39 +0200

    added more Values for Output + improved design

--- src/components/outputValue.js
+++ src/components/outputValue.js
@@ -1,18 +1,32 @@
-import  { Component } from "react";
-import OutputField from "./outputfield";
-
+import { Component } from "react";
 import styles from "./outputfield.module.css";
 
 class OutputValue extends Component {
-
-  
-    
-    render() {
+  render() {
+    return (
+      <div>
+        <div className={styles.content}>
+          <div className={styles.contentLeft}>
+            <div className={styles.image}>
+              <img src={this.props.icon} height={80}></img>
+            </div>
+            <div className={styles.temperature}>{this.props.temp}</div>
+            <div className={styles.celsius}> C°</div>
+            <div className={styles.list}>
+              <ol>{this.props.humidity}% Luftfeuchtigkeit</ol>
+              <ol>{this.props.wind} m/s</ol>
+              <ol>{this.props.cloudy}% Bewölkt</ol>
+            </div>
+          </div>
+          <div className={styles.contentRight}>
+            <div className={styles.locationName}>{this.props.name}</div>
+            <div className={styles.description}>{this.props.description}</div>
+          </div>
+        </div>
       
-      return (
-        <div className={styles.value}>{this.props.name}</div>    
-      );
-    }
+      </div>
+    );
   }
-  
-  export default OutputValue;
+}
+
+export default OutputValue;
--- src/components/outputfield.js
+++ src/components/outputfield.js
@@ -1,17 +1,14 @@
 import React, { Component } from "react";
-import styles from "./outputfield.module.css";
 
 import OutputValue from "./outputValue";
 
 class OutputField extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      temperatur: [],
+      weatherValues: [],
     };
   }
-
 
   getAllInformation = () => {
     let widget = [];
@@ -26,10 +23,9 @@ class OutputField extends Component {
   };
 
   loadCoordinates = (locationForApi) => {
-    
-    let temperatur = this.state.temperatur
-
-    if (locationForApi, temperatur) {
+    let weatherValue = this.state.weatherValues;
+    console.log(this.state.weatherValues);
+    if (locationForApi) {
       fetch(
         "https://api.openweathermap.org/geo/1.0/direct?limit=2&appid=77a92c772fa00a0ce8ac1d27c983fcdd&q=" +
           locationForApi
@@ -38,29 +34,31 @@ class OutputField extends Component {
           return response.json();
         })
         .then(function (data) {
-          if(data[0].name.toLowerCase() === locationForApi.toLowerCase() || data.length === 1){
+          if (
+            data[0].name.toLowerCase() || data[0].local_names.de.toLowerCase() === locationForApi.toLowerCase() ||
+            data.length === 1
+          ) {
             data = data[0];
-          }else{
+          } else {
             data = data[1];
           }
-          
-            fetch(
-              "https://api.openweathermap.org/data/2.5/weather?lat=" +
-                data.lat +
-                "&lon=" +
-                data.lon +
-                "&appid=77a92c772fa00a0ce8ac1d27c983fcdd&lang=de&units=metric"
-            )
-              .then(function (response) {
-                return response.json();
-              })
-              .then(function (data) {
-                 temperatur.push(data.main.temp);
-              })
-              .catch(function (err) {
-                console.log(err);
-              });
-          
+
+          fetch(
+            "https://api.openweathermap.org/data/2.5/weather?lat=" +
+              data.lat +
+              "&lon=" +
+              data.lon +
+              "&appid=77a92c772fa00a0ce8ac1d27c983fcdd&lang=de&units=metric"
+          )
+            .then(function (response) {
+              return response.json();
+            })
+            .then(function (data) {
+              weatherValue.push(data);
+            })
+            .catch(function (err) {
+              console.log(err);
+            });
         })
         .catch(function (err) {
           console.log(err);
@@ -68,24 +66,29 @@ class OutputField extends Component {
     }
   };
 
-  getValues(){
+  getValues() {
     let widgets = [];
 
-    
-
-    this.state.temperatur.forEach(temperatur =>{
-      widgets.push(<OutputValue name={temperatur}/>)
+    this.state.weatherValues.forEach((temperatur) => {
+      widgets = [];
+      widgets.push(
+        <OutputValue
+          name={temperatur.name}
+          temp={Math.round(temperatur.main.temp)}
+          country={temperatur.sys.country}
+          description={temperatur.weather[0].description}
+          icon={"http://openweathermap.org/img/w/" + temperatur.weather[0].icon + ".png"}
+          humidity={temperatur.main.humidity}
+          wind={temperatur.wind.speed}
+          cloudy={temperatur.clouds.all}
+        />
+      );
     });
     return widgets;
-}
+  }
 
   render() {
-    return (
-      this.loadCoordinates(this.getAllInformation()),
-      <div>
-        {this.getValues()}
-      </div>
-    );
+    return this.loadCoordinates(this.getAllInformation()), this.getValues();
   }
 }
 
